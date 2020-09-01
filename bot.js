30b491951c8b4221efcd354ae56c6c8c3cdb5a82
BodaRamzy300 <70328036+BodaRamzy300@users.noreply.github.com>
2020-09-01 04:42:25 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-"
+ 
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('         [Wait please .. ]       ')
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+client.on('ready', () => {
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[           BOT IS ONLINE         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[        info         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`servers! [ " ${client.guilds.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Users! [ " ${client.users.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`channels! [ " ${client.channels.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+ 
+client.user.setActivity('!help', { type: 'PLAYING' })
+});
+ 
+ 
+client.login('Your Token');
+
+client.on('message', message => {
+    if (message.content === "help") {
+message.channel.send(`**
+حط الرد هنا
+**`);
+      }
+});
